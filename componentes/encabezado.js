7abbb7badde6e8480ae01801cfb0519f867d38cc
PROES <proyectosproes@gmail.com>
2020-03-26 18:51:54 -0500

    Refactorización implementando Vuejs. Documentación ORM PIPE v3.0.0.

---
+++ componentes/encabezado.js
@@ -0,0 +1,52 @@
+//Desarrollado Por Juan Felipe Valencia Murillo.
+Vue.component('encabezado',{
+	data(){
+		return {
+			host:location.origin+'/pipe/'
+		}
+	},
+	mounted(){
+		this.seleccionarBotonCargado();
+	},
+	methods:{
+		seleccionarBotonCargado(){
+			if(location.href==this.host) this.asignarEstiloBoton(document.getElementById('inicio'));
+			if(location.href.indexOf('documentacion')>-1) this.asignarEstiloBoton(document.getElementById('documentacion'));
+			if(location.href.indexOf('descargar')>-1) this.asignarEstiloBoton(document.getElementById('descargar'));
+			if(location.href==this.host+'desarrollador/') this.asignarEstiloBoton(document.getElementById('desarrollador'));
+		},
+		asignarEstiloBoton(btn){
+			btn.style.color='#5BC800';
+			btn.style.borderTop='5px solid white';
+			btn.style.borderBottom='5px solid #5BC800';
+		}
+	},
+	template:`
+		<header>
+			<div class="logo">
+				<a :href="host"><img :src="host+'imagenes/eslogan_pipe.png'" alt="ESLOGAN PIPE" style="width:auto; height:50px; margin-top:10px;"/></a>
+				<!--<div class="titulo">PIPE</div>-->
+				<!--<div class="slogan">ORM en Español</div>-->
+			</div>
+			<div class="rutas">
+				<a :href="host"><button id="inicio"><i class="fas fa-home"></i> Inicio</button></a>
+				<a :href="host+'documentacion/3.x'"><button id="documentacion"><i class="fas fa-book"></i> Documentación</button></a>
+				<a :href="host+'descargar'"><button id="descargar"><i class="fas fa-download"></i> Descargar</button></a>
+				<a :href="host+'desarrollador'"><button id="desarrollador"><i class="far fa-user"></i> Desarrollador</button></a>
+			</div>
+		</header>
+	`
+});
+function abrirModal(id_modal){
+	modal=document.getElementById(id_modal);
+	modal.style.display='block';
+	window.onclick=function(event){
+		if(event.target==modal){
+			modal.style.display='none';
+		}
+	}
+}
+function cerrarModal(id_modal){
+	modal=document.getElementById(id_modal);
+	modal.style.display='none';
+}
---
+++ componentes/menu-documentacion.js
@@ -0,0 +1,76 @@
+//Desarrollado Por Juan Felipe Valencia Murillo.
+Vue.component('menu-documentacion',{
+	data(){
+		return {
+			href:location.href,
+			host:location.origin+'/pipe/',
+			menu:[],
+			menuVersion:{},
+			versiones:[
+				'v3.x',
+				'v2.x',
+				'v1.x'
+			]
+		}
+	},
+	mounted(){
+		this.asignarMenu('v3.x');
+		this.asignarMenuVersion('v3.x');
+	},
+	methods:{
+		asignarMenu(version){
+			this.menu=[
+				{href:this.host+'documentacion/_version_/instalacion/',texto:'Instalación'},
+				{href:this.host+'documentacion/_version_/configuracion/',texto:'Configuración'},
+				{href:this.host+'documentacion/_version_/convenciones/',texto:'Convenciones'},
+				{href:this.host+'documentacion/_version_/modelos-y-constructor-de-consultas/',texto:'Modelos y Constructor de Consultas'},
+				{href:this.host+'documentacion/_version_/insertar/',texto:'Insertar'},
+				{href:this.host+'documentacion/_version_/consultar/',texto:'Consultar'},
+				{href:this.host+'documentacion/_version_/actualizar/',texto:'Actualizar'},
+				{href:this.host+'documentacion/_version_/eliminar/',texto:'Eliminar'},
+				{href:this.host+'documentacion/_version_/vaciar-tabla/',texto:'Vaciar Tabla'},
+				{href:this.host+'documentacion/_version_/autenticar-usuarios/',texto:'Autenticar Usuarios'}
+			];
+			version=this.obtenerVersionURL(version).substr(1,3);
+			this.menu.forEach(function(item){
+				item.href=item.href.replace('_version_',version);
+			});
+		},
+		asignarMenuVersion(version){
+			version=this.obtenerVersionURL(version);
+			this.eliminarVersion(version);
+			this.menuVersion={
+				actual:version,
+				versiones:this.versiones
+			}
+		},
+		eliminarVersion(version){
+			let index=this.versiones.indexOf(version);
+			if(index>-1) this.versiones.splice(index,1);
+		},
+		obtenerVersionURL(defecto=null){
+			let version=defecto;
+			let index=location.href.indexOf('.x');
+			if(index>-1) version='v'+location.href.substr(index-1,3);
+			return version;
+		}
+	},
+	template:`
+	<div class="menu-doc">
+	<ul>
+		<li><a style="cursor:pointer;">{{menuVersion.actual}} <i class="fa fa-angle-down"></i></a>
+			<ul>
+				<li v-for="(item, index) in menuVersion.versiones">
+					<a :href="host+'documentacion/'+item.substr(1,3)">{{item}}</a>
+				</li>
+			</ul>
+		</li>
+	</ul>
+	<div class="hr"></div>
+	<template v-for="(item, index) in menu">
+		<a :href="item.href" class="menu_link" :style="href==item.href ? 'color:#5BC800; font-weight:bold;' : ''">{{item.texto}}</a>
+		<div class="hr"></div>
+	</template>
+	</div>
+	`
+});
